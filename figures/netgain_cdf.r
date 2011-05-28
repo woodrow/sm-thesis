8ca72c0b0b912a755abf3dbad249d804b820ecfb
Stephen Woodrow <srwoodrow@gmail.com>
2011-05-27 22:03:45 -0400

    Reformat figure, add threshold lines, and add new plot types.

--- figures/netgain_cdf.r
+++ figures/netgain_cdf.r
@@ -3,8 +3,8 @@ netgain_cdf_gcr <- read.csv("netgain_cdf_gcr.csv")
 netgain_cdf_gcr$netgain = netgain_cdf_gcr$nets_reduced
 netgain_cdf_gcr$netsnow = netgain_cdf_gcr$nets_current
 
-showpoints=T
-verticals=F
+showpoints=F
+verticals=T
 lwd=2
 cex=0.75
 #log="x"
@@ -19,14 +19,22 @@ plot_netgain_cdf_acr <- function(log='') {
 plot_netgain_cdf_gcr <- function(log='', ylims=c(0.9,1), printTOP30=F) {
     plot_netgain_cdf(netgain_cdf_gcr, 'netgain', log, ylims, printTOP30)
 }
- 
+
 plot_netsnow_cdf_acr <- function(log='') {
     plot_netgain_cdf(netgain_cdf_acr, 'netsnow', log)
 }
 
 plot_netsnow_cdf_gcr <- function(log='', ylims=c(0.9,1), printTOP30=F) {
     plot_netgain_cdf(netgain_cdf_gcr, 'netsnow', log, ylims, printTOP30)
-} 
+}
+
+plot_frac_aggr_cdf_gcr <- function(log='', ylims=c(0.6,1), printTOP30=F) {
+    netgain_cdf_gcr$frac_aggr = as.numeric(netgain_cdf_gcr$netgain)/as.numeric(netgain_cdf_gcr$netsnow)
+    print(length(netgain_cdf_gcr$frac_aggr))
+    print(length(factor(netgain_cdf_gcr$frac_aggr)))
+    plot_netgain_cdf(netgain_cdf_gcr, 'frac_aggr', log, ylims, printTOP30)
+}
+
 
 plot_netgain_cdf <- function(d, series='netgain', log='', ylims=c(0,1), printTOP30=F) {
     par(cex=cex)
@@ -34,20 +42,26 @@ plot_netgain_cdf <- function(d, series='netgain', log='', ylims=c(0,1), printTOP
     d$date = 1900 + as.POSIXlt(d$date)$year
     yrs = split(d, f=as.factor(d$date))
     jet.colors = colorRampPalette(
-        c("#00007F", "blue", "#007FFF", "cyan", 
+        c("#00007F", "blue", "#007FFF", "cyan",
         "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
     colvec = jet.colors(length(yrs))
     names(colvec) = names(yrs)
     print_axis=T
     xlabs = c(netgain="Aggregable prefixes (netgain)",
-        netsnow="Total advertised prefixes (netsnow)")
-    xlims = c(max(1,min(d[[series]])),max(d[[series]]))
+        netsnow="Total advertised prefixes (netsnow)",
+        frac_aggr="Fraction of announced prefixes that are aggregable")
+    print(xlabs)
+    if(log=='x') {
+        xlims = c(max(1,min(d[[series]])),max(d[[series]]))
+    } else {
+        xlims = c(min(d[[series]]),max(d[[series]]))
+    }
 #    ylims = c(0.99,1)
     for(y in names(yrs)) {
         print(paste("max:", y, max(yrs[[y]][[series]])))
         print(paste("min:", y, min(yrs[[y]][[series]])))
         if(print_axis) {
-             plot.ecdf(
+            plot.ecdf(
                 yrs[[y]][[series]],
                 main="",
                 xlab=xlabs[series],
@@ -61,7 +75,7 @@ plot_netgain_cdf <- function(d, series='netgain', log='', ylims=c(0,1), printTOP
                 #xaxp=c(0,1,10),
                 col=colvec[y]
                 )
-             print_axis=F
+            print_axis=F
         } else {
             plot.ecdf(
                 yrs[[y]][[series]],
@@ -77,7 +91,8 @@ plot_netgain_cdf <- function(d, series='netgain', log='', ylims=c(0,1), printTOP
                 ylim=ylims,
                 log=log,
                 #xaxp=c(0,1,10),
-                col=colvec[y]
+                col=colvec[y],
+                add=T
                 )
         }
         if(printTOP30) {
@@ -85,7 +100,6 @@ plot_netgain_cdf <- function(d, series='netgain', log='', ylims=c(0,1), printTOP
             abline(h=hpos)
             text(1, hpos, paste("Top 30,", y), adj=c(0,2), cex=cex)
         }
-        par(new=T)
     }
     grid()
     par(new=F)
@@ -99,13 +113,15 @@ plot_netgain_cdf <- function(d, series='netgain', log='', ylims=c(0,1), printTOP
         bg="white",
         inset=0.02
         )
-    if(series == 'etgain' && length(yrs[[1]][[series]]) > 30) {
+    if(series == 'netgain' && length(yrs[[1]][[series]]) > 30) {
         hpos=1-(30/length(yrs[[1]][[series]]))
-        abline(h=hpos)
-        text(1, hpos, paste("Top 30,", names(yrs)[1]), adj=c(0,2), cex=cex)
+        col='black'
+        abline(h=hpos, col=col)
+        text(1, hpos, paste("Top 30,", names(yrs)[1]), adj=c(0,1.3), cex=cex, col=col)
         hpos=1-(30/length(yrs[[length(yrs)]][[series]]))
-        abline(h=hpos)
-        text(1, hpos, paste("Top 30,", names(yrs)[length(yrs)]), adj=c(0,2), cex=cex)
+        col='red3'
+        abline(h=hpos, col=col)
+        text(1, hpos, paste("Top 30,", names(yrs)[length(yrs)]), adj=c(0,-0.3), cex=cex, col=col)
     }
 }
 
@@ -119,4 +135,16 @@ pdf_netsnow_cdf_gcr <- function() {
     pdf("netgain_netsnow_cdf_gcr.pdf", width=6, height=3)
     plot_netsnow_cdf_gcr(log='x', ylims=c(0.75,1))
     dev.off()
-} 
+}
+
+pdf_netgain_cdf_gcr <- function() {
+    pdf("netgain_cdf_gcr.pdf", width=6, height=3)
+    plot_netgain_cdf_gcr(log='x', ylims=c(0.75,1.005))
+    dev.off()
+}
+
+pdf_fracaggr_cdf_gcr <- function() {
+    pdf("netgain_frac_aggr_cdf_gcr.pdf", width=6, height=3)
+    plot_frac_aggr_cdf_gcr()
+    dev.off()
+}
